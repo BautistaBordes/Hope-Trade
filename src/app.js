e20c3f4e084d6838d39359ac0ee38ce1caffc3f8
martin-marzer <martinvazquez1104@gmail.com>
2024-05-15 16:03:43 -0300

    visuales login/register

--- src/app.js
+++ src/app.js
@@ -3,6 +3,25 @@ const app = express();
 const path = require('path');
 const session = require('express-session');
 const PORT = 8080;
+const db = require('./database/index');
+
+const connectAndSync = async () => {
+  try {
+    //esto es para iniciar la conexion con la bd
+    await db.authenticate();
+    //esto es para sincronizar TODOS los modelos definidos (carpeta models) con las tablas de la BD
+    await db.sync();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    //error mas comun: ConnectionRefusedError, no levantaste la bd con xxamp
+  }
+};
+
+//si no agrego esto no se contemplan las asociaciones 
+require('./database/associations');
+connectAndSync();
+
 
 //aca seteas en que carpeta tenes todos archivos que no van a cambiar (podes agregar o eliminar, pero no modificar un file)
 app.use(express.static("public"));
@@ -16,7 +35,10 @@ app.use(express.json());
 app.use(session({
   secret: "shh",
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  // cookie: {
+  //   maxAge:  10 * 1000 //con esto digo por cuanto tiempo quiero que dure, sino queda hasta que cierras el navegador
+  // }
 }))
 
 // set our default template engine to "ejs"
@@ -26,9 +48,9 @@ app.set("view engine", "ejs");
 // setea las vistas, lo que esta entre [] indica en que carpeta estan
 app.set("views", [path.join(__dirname, 'views')]);
 
-const frontBackMiddleware = require("./middlewares/frontBackMiddleware")
+const frontToBackMiddleware = require("./middlewares/frontToBackMiddleware")
 
-app.use(frontBackMiddleware)
+app.use(frontToBackMiddleware)
 
 //importas las rutas que va a usar en la app
 const rutaMain = require("./routes/main")
@@ -49,8 +71,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('se rompio todo');
 });
 
-
 // arranca el servidor
 app.listen(PORT, () => {
-    console.log(`funca bien pa, ${PORT} server personal`);
+    console.log(`aplicacion funcionando en el puerto ${PORT}, server personal`);
 })
