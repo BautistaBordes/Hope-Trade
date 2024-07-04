56182a9392bd7bab12178e556ee46421b9e9e295
brunosanan <bruno.sanandres04@gmail.com>
2024-07-04 03:25:53 -0300

    model tarjeta sequelize

---
+++ src/database/models/Tarjeta.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../index');
+
+
+let alias = 'Tarjeta'; // esto debería estar en singular
+
+let cols = {
+    numero: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        allowNull: false
+    },
+
+    nombre: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+
+    cdo_seguridad: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+
+    vencimiento: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+
+    credito: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+};
+
+let config = {
+    tableName: "tarjeta"
+}
+
+const Tarjeta = sequelize.define(alias, cols, config);
+
+module.exports = Tarjeta;
